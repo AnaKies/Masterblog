7a7086446d8fec584cce582eecf995550e9b2c94
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-06-23 16:52:40 +0200

    Save the entered data in the JSON file.

--- app.py
+++ app.py
@@ -1,44 +1,45 @@
 from flask import Flask, render_template, request
-import json
+import json_manager.json_handler as json_handler
+import random
 
 
 app = Flask(__name__)
-
-
-def read_json():
-    """
-    Retrieves the blog posts data from the JSON storage.
-    """
-    try:
-        with open('storage/posts.json') as json_file:
-            json_data = json.load(json_file)
-        return json_data
-    except Exception as error:
-        print(f"Error getting JSON data: {error}")
-        return {}
-
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     """
     Asks the user to enter details for a new blog post.
     """
-    if request.method == 'POST':
-        user_name = request.form['user_name']
-        post_title = request.form['post_title']
-        post_content = request.form['post_content']
-        return render_template('add.html', data_submited=True,
-                               user_name=user_name,
-                               post_title=post_title,
-                               post_content=post_content)
-    # case for GET request
-    return render_template('add.html', data_submited=False)
+    try:
+        if request.method == 'POST':
+            user_name = request.form['user_name']
+            post_title = request.form['post_title']
+            post_content = request.form['post_content']
+            user_id = random.randint(100000, 999999)
+            new_post = {'id': user_id, 'author': user_name, 'title': post_title, 'content': post_content}
+            data = json_handler.read_json()
+            if not data:
+                data = []
+            data.append(new_post)
+            json_handler.write_json(data)
+
+            return render_template('add.html', data_submited=True,
+                                   user_name=user_name,
+                                   post_title=post_title,
+                                   post_content=post_content)
+        # case for GET request
+        return render_template('add.html', data_submited=False)
+    except Exception as error:
+        print(f"Error adding blog post data: {error}")
+        return []
 
 
 
 @app.route('/')
 def index():
-    blog_posts = read_json()
+    blog_posts = json_handler.read_json()
+    if not blog_posts:
+        blog_posts = []
     return render_template('index.html', posts=blog_posts)
 
 
---
+++ json_manager/json_handler.py
@@ -0,0 +1,32 @@
+import json
+
+
+def read_json():
+    """
+    Retrieves the blog posts data from the JSON storage.
+    """
+    try:
+        with open('json_storage/posts.json', 'r') as json_file:
+            # a file can be empty but containing a formatting signs, spaces -> strip()
+            raw_data = json_file.read().strip()
+            if not raw_data:
+                print('Warning: empty JSON storage!')
+                return []
+            json_data = json.loads(raw_data)
+        return json_data
+    except Exception as error:
+        print(f"Error reading blog post data: {error}")
+        return None
+
+
+def write_json(data):
+    """
+    Writes the blog posts data into the JSON storage.
+    """
+    try:
+        with open('json_storage/posts.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+            return True
+    except Exception as error:
+        print(f"Error writing blog post data: {error}")
+        return None
